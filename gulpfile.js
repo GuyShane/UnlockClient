909c3f3113bb0b8e89c7ba8aaa71efe60d2f26e2
Shane <shanebrass1618@gmail.com>
2018-03-17 14:31:39 -0600

    Adding css autoprefix to gulp process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp=require('gulp');
 const uglify=require('gulp-uglify');
 const sass=require('gulp-sass');
+const prefix=require('gulp-autoprefixer');
 const rename=require('gulp-rename');
 const pump=require('pump');
 
@@ -37,15 +38,23 @@ gulp.task('min-scss', ()=>{
     ]);
 });
 
+gulp.task('prefix', ['scss', 'min-scss'], ()=>{
+    pump([
+        gulp.src(['./src/unlock.css', './src/unlock.min.css']),
+        prefix(),
+        gulp.dest('./src/')
+    ]);
+});
+
 gulp.task('watch-js', ()=>{
     return gulp.watch('./src/unlock.js', ['uglify']);
 });
 
 gulp.task('watch-css', ()=>{
-    return gulp.watch('./src/unlock.scss', ['scss', 'min-scss']);
+    return gulp.watch('./src/unlock.scss', ['scss', 'min-scss', 'prefix']);
 });
 
 gulp.task('js', ['uglify', 'watch-js']);
-gulp.task('css', ['scss', 'min-scss', 'watch-css']);
+gulp.task('css', ['scss', 'min-scss', 'prefix', 'watch-css']);
 
 gulp.task('default', ['js', 'css']);
