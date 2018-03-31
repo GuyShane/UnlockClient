1f275b7bd111f260a1c14b1cf6d9d45887fb6425
Shane <shanebrass1618@gmail.com>
2018-03-31 11:26:57 -0600

    Adding module compatibility

--- src/unlock.js
+++ src/unlock.js
@@ -1,4 +1,12 @@
-(function(){
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports=factory();
+    } else {
+        root.Unlock=factory();
+    }
+}(this, function() {
     function Unlock(opts){
         var self=this;
 
@@ -273,5 +281,5 @@
         return s;
     }
 
-    window.Unlock=Unlock;
-})();
+    return Unlock;
+}));
